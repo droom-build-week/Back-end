99065e1e31ad048f51fdf72557cf3eab121a5bec
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-02-04 15:23:11 +0100

    changed Evans suggestions 2

--- users/getAllUsers.js
+++ users/getAllUsers.js
@@ -1,15 +1,20 @@
-const Users = require('./users-model');
+const Users = require("./users-model");
 
 function getAllUsers(req, res) {
   Users.find()
     .then(users => {
-      res.status(200).json(users)
+      if (users) {
+        res.status(200).json(users);
+      } else {
+        res.status(404).json({
+          message: "There was an error retrieving all users"
+        });
+      }
     })
     .catch(err => {
-      res.status(404).json({
-        message: err.message,
-        stack: err.stack
-      })
+      res.status(500).json({
+        message: "There was an error retrieving all users"
+      });
     });
 }
 
